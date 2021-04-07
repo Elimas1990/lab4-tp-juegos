08b1594b7233dd978758c1f5106eeb5816c0e68e
Elimas1990 <estebanlimas@gmail.com>
2021-04-07 13:55:14 -0300

    agregue firebase a prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDXGIDS7To0FdPGGVHe9I1QmzuaFexLQ28",
+    authDomain: "tp-saladejuegos-98240.firebaseapp.com",
+    projectId: "tp-saladejuegos-98240",
+    storageBucket: "tp-saladejuegos-98240.appspot.com",
+    messagingSenderId: "302454874437",
+    appId: "1:302454874437:web:d7d548ae79ff46d4f3de2c"
+  }
 };
